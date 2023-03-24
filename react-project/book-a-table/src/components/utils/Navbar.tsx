593e15443310714ae42bdaa5823cbfe25371574f
JuankGF <juankgonzalezf@gmail.com>
2023-03-24 15:24:37 -0400

    Extracting Navbar component

---
+++ react-project/book-a-table/src/components/utils/Navbar.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <ul className="nav-menu">
+      <li>
+        <Link className="app-link" to={"/"}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="app-link" to={"/about"}>
+          About
+        </Link>
+      </li>
+      <li>
+        <Link className="app-link" to={"/menu"}>
+          Menu
+        </Link>
+      </li>
+      <li>
+        <Link className="app-link" to={"/reservations"}>
+          Reservations
+        </Link>
+      </li>
+      <li>
+        <Link className="app-link" to={"/login"}>
+          Login
+        </Link>
+      </li>
+      <li className="avatar">
+        <Image
+          alt="avatar"
+          roundedCircle
+          src="https://api.dicebear.com/5.x/fun-emoji/svg?seed=avatar"
+          width="45vmin"
+        ></Image>
+      </li>
+    </ul>
+  );
+}
