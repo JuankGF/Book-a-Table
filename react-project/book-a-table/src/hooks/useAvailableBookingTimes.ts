0b48767d992e5d162f947c6065010c2e2434fc17
JuankGF <juankgonzalezf@gmail.com>
2023-03-29 10:11:35 -0400

    Validate date value. Add list available times hook

--- react-project/book-a-table/src/hooks/useAvailableBookingTimes.ts
+++ react-project/book-a-table/src/hooks/useAvailableBookingTimes.ts
@@ -9,27 +9,45 @@ const getRandomIntInclusive = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const useAvailableTimes = (date: Date) => {
-  const formatedDate = format(date, "mm/dd/YYYY");
-
+const useAvailableTimes = (date?: Date) => {
+  const formatedDate = date ? format(date, "MM/dd/yyyy") : "";
   const { availableTimes } = useMemo(() => {
     /* Create new Map to get available times */
     const availableTimes = new Map<string, Set<string>>();
-    /* Add start and end random values */
-    const randomStart = getRandomIntInclusive(0, 23);
-    const randomEnd = getRandomIntInclusive(randomStart, 23);
-    /* Pick some random def times */
-    const randomTimes = defBookingTimes.slice(randomStart, randomEnd);
-    /* Check if exist record for specific date */
-    const dateBookingTimes = availableTimes.get(formatedDate);
-    /* Apply modifications to data */
-    if (dateBookingTimes?.size) {
-      randomTimes.forEach((time) => dateBookingTimes.add(time));
-    } else availableTimes.set(formatedDate, new Set(randomTimes));
+    if (formatedDate) {
+      /* Add start and end random values */
+      const randomStart = getRandomIntInclusive(0, 23);
+      const randomEnd = getRandomIntInclusive(randomStart, 23);
+      /* Pick some random def times */
+      let randomTimes = [""];
+      randomTimes = randomTimes.concat(
+        defBookingTimes.slice(randomStart, randomEnd)
+      );
+      /* Check if exist record for specific date */
+      const dateBookingTimes = availableTimes.get(formatedDate);
+      /* Apply modifications to data */
+      if (dateBookingTimes?.size) {
+        randomTimes.forEach((time) => dateBookingTimes.add(time));
+      } else availableTimes.set(formatedDate, new Set(randomTimes));
+    }
 
     return { availableTimes };
   }, [formatedDate]);
-  return availableTimes;
+  return { availableTimes };
 };
 
-export { useAvailableTimes };
+const useListTimes = (
+  availableTimes: Map<string, Set<string>>,
+  pickedDate: string
+) => {
+  const timeList = useMemo(() => {
+    const list = [] as string[];
+    availableTimes.get(pickedDate)?.forEach((time) => {
+      list.push(time);
+    });
+    return list;
+  }, [availableTimes, pickedDate]);
+  return timeList;
+};
+
+export { useAvailableTimes, useListTimes };
