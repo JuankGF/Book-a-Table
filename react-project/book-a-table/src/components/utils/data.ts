518354d3c664bcfc0ee237ef293027bcbed55f75
JuankGF <juankgonzalezf@gmail.com>
2023-03-29 09:07:26 -0400

    Added default booking times

--- react-project/book-a-table/src/components/utils/data.ts
+++ react-project/book-a-table/src/components/utils/data.ts
@@ -13,6 +13,37 @@ type Dish = {
   price: number;
 };
 
+export type BookingTimes = {
+  date: string /* mm/dd/YYYY */;
+  availableTimes: string[] /* HH:mm P */;
+};
+
+const defBookingTimes = [
+  "11:00 AM",
+  "11:30 AM",
+  "12:00 PM",
+  "12:30 PM",
+  "13:00 PM",
+  "13:30 PM",
+  "14:00 PM",
+  "14:30 PM",
+  "15:00 PM",
+  "15:30 PM",
+  "16:00 PM",
+  "16:30 PM",
+  "17:00 PM",
+  "17:30 PM",
+  "18:00 PM",
+  "18:30 PM",
+  "19:00 PM",
+  "19:30 PM",
+  "20:00 PM",
+  "20:30 PM",
+  "21:00 PM",
+  "21:30 PM",
+  "22:00 PM",
+];
+
 const dishList = [
   {
     image:
@@ -115,4 +146,4 @@ const testimonials = [
   },
 ] as Testimonial[];
 
-export { dishList, testimonials };
+export { dishList, testimonials, defBookingTimes };
